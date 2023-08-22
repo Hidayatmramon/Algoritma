d16be6a0895a131163bf81bf7c170e76eea4ce6f
ramon <hidayatmramon@gmail.com>
2023-08-22 18:20:34 +0700

    update and edit coding

---
+++ PHP/no8.php
@@ -0,0 +1,16 @@
+<?php
+
+
+     $kecepatan = 2;
+     $jarak = 100;
+     $detik;
+
+    $detik = $jarak / $kecepatan;
+
+    echo "Untuk menenmpuh jarak ", $jarak, " M maka waktu yang diperlukan adalah ", $detik, " Detik ";
+
+
+
+
+
+?>
